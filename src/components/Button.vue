9964d82d18cd00f0bfcf6fc256dc41754f4924b0
Rajaram Panda <rpanda@argusoft.in>
2021-06-02 11:25:42 +0530

    changes

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button v-on:click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "New Button",
+    },
+    color: {
+      type: String,
+      default: "blue",
+    },
+  },
+  methods: {
+    onClick() {
+      console.log("button clicked");
+    },
+  },
+  //   props: ["text", "color"],
+};
+</script>
---
+++ src/components/Task.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="task">
+    <h3>{{ task.text }}</h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Task",
+  props: ["task"],
+};
+</script>
